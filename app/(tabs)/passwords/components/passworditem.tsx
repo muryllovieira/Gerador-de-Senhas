28ad7e1b9e8a762c866c20add3adb80c0e2ab07f
muryllovieira <muryllovieira59@gmail.com>
2025-02-20 15:25:53 -0300

    Gerador de Senhas Finalizado

--- app/(tabs)/passwords/components/passworditem.tsx
+++ app/(tabs)/passwords/components/passworditem.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export function PasswordItem({ data, removePassword }){
+export function PasswordItem({ data, removePassword, onIconPress }) {
+    // Estado para controlar se a senha deve ser visível ou não
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    // Função de alternância do estado de visibilidade da senha
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+        if (onIconPress) {
+            onIconPress(!isPasswordVisible); // Passando o estado de visibilidade para o onIconPress, se necessário
+        }
+    };
+
     return (
         <Pressable onLongPress={removePassword} style={styles.container}>
-            <Text style={styles.text}>{data}</Text>
+            <Text style={styles.text}>{isPasswordVisible ? data : '********'}</Text>
+            <Pressable onPress={togglePasswordVisibility}>
+                <MaterialCommunityIcons 
+                    name={isPasswordVisible ? 'eye' : 'eye-off'} 
+                    size={30} 
+                    color={"#FFF"} 
+                />
+            </Pressable>
         </Pressable>
     );
 }
@@ -26,4 +45,4 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textTransform: "uppercase",
     }
-})
+});
--- app/(tabs)/passwords/index.tsx
+++ app/(tabs)/passwords/index.tsx
@@ -39,6 +39,7 @@ export default function Passwords() {
                         handleDeletePasswords(item)
                     }}/>}
                 />
+            
             </View>
         </SafeAreaView>
 
